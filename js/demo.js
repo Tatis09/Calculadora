ede7e1c2c59702b7e7827b319a2291a93e135e4e
Tatiana Parada Gómez <paradagomeztatiana3@gmail.com>
2018-07-04 21:30:22 -0500

    Hola

--- js/demo.js
+++ js/demo.js
@@ -1,131 +1,13 @@
-/*var operandoa;
-var operandob;
-var operacion;
-
-function init(){
-  //variables
-  var resultado = document.getElementById('resultado');
-  var reset = document.getElementById('reset');
-  var suma = document.getElementById('suma');
-  var resta = document.getElementById('resta');
-  var multiplicacion = document.getElementById('multiplicacion');
-  var division = document.getElementById('division');
-  var igual = document.getElementById('igual');
-  var uno = document.getElementById('uno');
-  var dos = document.getElementById('dos');
-  var tres = document.getElementById('tres');
-  var cuatro = document.getElementById('cuatro');
-  var cinco = document.getElementById('cinco');
-  var seis = document.getElementById('seis');
-  var siete = document.getElementById('siete');
-  var ocho = document.getElementById('ocho');
-  var nueve = document.getElementById('nueve');
-  var cero = document.getElementById('cero');
-}
-
-//Eventos de click
-var a=[1,2,3,4,5,6,7,8,9];
- for (x=0; x<a.length; x++)
-
-uno.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "1";
-}
-dos.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "2";
-}
-tres.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "3";
-}
-cuatro.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "4";
-}
-cinco.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "5";
-}
-seis.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "6";
-}
-siete.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "7";
-}
-ocho.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "8";
-}
-nueve.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "9";
-}
-cero.onclick = function(e){
-    resultado.textContent = resultado.textContent  + "0";
-}
-reset.onclick = function(e){
-    resetear();
-}
-suma.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "+";
-    limpiar();
-}
-resta.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "-";
-    limpiar();
-}
-multiplicacion.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "*";
-    limpiar();
-}
-division.onclick = function(e){
-    operandoa = resultado.textContent;
-    operacion = "/";
-    limpiar();
-}
-igual.onclick = function(e){
-    operandob = resultado.textContent;
-    resolver();
-}
-
-function limpiar(){
-    resultado.textContent = "";
-  }
-  function resetear(){
-    resultado.textContent = "";
-    operandoa = 0;
-    operandob = 0;
-    operacion = "";
-  }
-
-  function resolver(){
-    var res = 0;
-    switch(operacion){
-      case "+":
-        res = parseFloat(operandoa) + parseFloat(operandob);
-        break;
-      case "-":
-          res = parseFloat(operandoa) - parseFloat(operandob);
-          break;
-      case "*":
-        res = parseFloat(operandoa) * parseFloat(operandob);
-        break;
-      case "/":
-        res = parseFloat(operandoa) / parseFloat(operandob);
-        break;
-    }
-    resetear();
-    resultado.textContent = res;
-  }
-  */
-
-  function solonumeros(e) {
+    function solonumeros(e){
     
-    key = e.keyCode || e.witch;
-    teclado =String.fromCharCode(Key);
-    numeros ="0123456789";
-    especiales ="8-37-39-46";
+    key=e.keyCode || e.which;
+    teclado=String.fromCharCode(Key);
+    numeros="0123456789";
+    especiales="8-37-39-46";
 
-    tecla_especial= false;
+    tecla_especial=false;
 
-    for (var i in especiales){
+    for(var i in especiales){
         if(key==especiales[i]){
             tecla_especial= true;
         }
@@ -139,9 +21,9 @@ function limpiar(){
 
   function retornar(num){
 
-    var anterior = document.fo.valores.value;
+    var anterior=document.fo.valores.value;
 
     document.getElementById ("valores").value = anterior+num;
   }
 
- 
+  function eliminar()
